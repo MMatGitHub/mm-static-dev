e7cd4194a60cbdf76ea2c6869f13fbb160a9e8b6
MMatGitHub <mima-ffm@gmx.de>
2023-09-05 17:53:49 +0200

    Create messwerte.js

---
+++ mie/js/messwerte.js
@@ -0,0 +1,16 @@
+const messwerte_as_json_obj= [
+      { id: "t1", wert: 1468, zeit: "Jan" },
+      { id: "t2", wert: 1494, zeit: "Feb" },
+      { id: "t3", wert: 1540, zeit: "Mrz" },
+      { id: "t4", wert: 1592, zeit: "Apr" },
+      { id: "t5", wert: 1638, zeit: "Mai" }
+];
+
+function ausgeben(){
+   /* for (const item of messwerte_as_json) {
+        console.log (item.id);
+        for (const [index, books] of item.books.entries()) {
+            console.log (`${index} . ${books}`);
+        }
+    }*/
+}
