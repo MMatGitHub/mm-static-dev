c14a8c634118a965d74d948c3f0ee7d712ef979e
MMatGitHub <mima-ffm@gmx.de>
2023-09-07 12:46:17 +0200

    index.js  added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-window.location.assign("./index.html");
+import './mm.css';
+import { version, name } from './package.json';
+
+document.getElementById('app_author').innerHTML = `<p>Author: Duomilia</p>`;
+document.getElementById('app_author').innerHTML = '<p>Name: ' + name + '</p>';
+document.getElementById('app_version').innerHTML =
+  '<p>Version: ' + version + '</p>';
